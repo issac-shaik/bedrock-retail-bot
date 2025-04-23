02506172978293ba82a0b303c2fac73cfd427215
Issac Shaik <issac.shaik@gmail.com>
2025-04-23 23:44:02 +0530

    create DynamoDB tables

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,1 +1,21 @@
- 
+resource "aws_dynamodb_table" "customers" {
+  name           = "Customers"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "customerId"
+
+  attribute {
+    name = "customerId"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "inventory" {
+  name           = "Inventory"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "productId"
+
+  attribute {
+    name = "productId"
+    type = "S"
+  }
+}
--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,1 +1,7 @@
- 
+output "customers_table_name" {
+  value = aws_dynamodb_table.customers.name
+}
+
+output "inventory_table_name" {
+  value = aws_dynamodb_table.inventory.name
+}
